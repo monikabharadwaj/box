88adee1703113c0a5ff11dd0b2bc108410cbc7ef
monikabharadwaj <72626735+monikabharadwaj@users.noreply.github.com>
2021-12-01 08:52:37 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,10,10)
+box.shapeColor="cyan"
+}
+
+function draw() 
+{
+ background("skyblue")
+
+drawSprites()
+}
+
+
+
+
